e2a3f10ea0fba08270626a570423255393e82aa1
Tamagochi03 <zielhafen.ipn@hotmail.com>
2015-02-19 03:15:43 -0600

    Se crea clase EstadoGeneral.java
    Esta clase se crea para optimizar el código, ya que las clases de que implementan la interfaz Estado también heredan de EstadoGeneral, en la cual únicamente se definen los atributos que tienen los estados, independientemente de su implementación.

---
+++ EstadoGeneral.java
@@ -0,0 +1,16 @@
+package estados;
+
+import javax.media.j3d.TransformGroup;
+import javax.swing.JLabel;
+import logica.Tamagochi;
+
+/**
+ *
+ * @author Shinsnake
+ */
+public class EstadoGeneral {
+    private TransformGroup tg;
+    private Tamagochi tama;
+    private int energia;
+    JLabel mensajePensamiento;
+}
